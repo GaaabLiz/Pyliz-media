058bdb510a78d0d399e93a8cd6686b55f91f76ac
gabliz <lizzosgabriele@gmail.com>
2025-02-08 15:22:35 +0100

    updated project README.md

--- README.md
+++ README.md
@@ -5,3 +5,8 @@ This is a simple python library that contains some useful functions regarding me
 ```bash
 pip install pylizmedia
 ```
+
+## Description
+The purpose of the library is to provide:
+- A class used for handling media files and directories for a python script/project.
+- Some general utilities regarding video frames and audio.
--- pylizmedia/liz_media.py
+++ pylizmedia/liz_media.py
@@ -129,12 +129,3 @@ class LizMedia:
         self.ai_tags = ai_info.tags
         self.ai_scanned = True
 
-
-class LizMediaExporter:
-
-    def __init__(self, media: LizMedia):
-        self.media = media
-
-    def export_to_json(self):
-        fileutils.write_json_to_file(self.media.path, self.media.file_name + ".json", self.media.to_json_only_ai())
-
